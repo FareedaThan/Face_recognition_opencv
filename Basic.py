4a7b545cc183281f4d760b89f6feaec2d66a3b91
FareedaThan <52806480+FareedaThan@users.noreply.github.com>
2021-03-11 21:31:52 +0700

    add face detection

--- Basic.py
+++ Basic.py
@@ -10,6 +10,18 @@ imgKang = cv2.cvtColor(imgKang,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('imageBasic/Kang test.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
+# detect face return bounding box
+faceLocation = face_recognition.face_locations(imgKang)[0]
+
+
+# embedding face
+encodeKang = face_recognition.face_encodings(imgKang)[0]
+
+# show bounding box location left bottom, right top
+cv2.rectangle(imgKang, (faceLocation[3], faceLocation[0]), (faceLocation[1], faceLocation[2]), (255,0,255),2)
+print(faceLocation)
+
+
 # show image
 cv2.imshow('Kang Daniel', imgKang)
 cv2.imshow('Kang Test', imgTest)
